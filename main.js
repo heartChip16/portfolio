c68818fc66b5404a88d9951b2690fca0f31e359a
Ingrid E. Torres <ingrid.escabal@gmail.com>
2022-11-23 10:08:17 +0800

    changed a lot in design

--- main.js
+++ main.js
@@ -1,38 +1,38 @@
-const dynamicContent = document.getElementById("dynamic-text") ;
-const phrases = ["an Electronics Engineer." ,"a Freelancer.", "a Web Developer."];
+const dynamicContent = document.getElementById("dynamic-text");
+const phrases = ["an Electronics Engineer.", "a Freelancer.", "a Web Developer."];
 const typingSpeed = 300;
 const erasingSpeed = 75;
 let letterIndex = 0;
-let phrasesIndex=0;
+let phrasesIndex = 0;
 
 
-function printLetters(phrase){
-    if(letterIndex==phrase.length) {
+function printLetters(phrase) {
+    if (letterIndex == phrase.length) {
         clearLetters();
     }
-    else if(letterIndex <phrase.length){
+    else if (letterIndex < phrase.length) {
         dynamicContent.textContent += phrase.charAt(letterIndex);
-        letterIndex +=1;
-        setTimeout(function(){
+        letterIndex += 1;
+        setTimeout(function () {
             printLetters(phrase)
-        },typingSpeed);
+        }, typingSpeed);
     }
 
 }
 
-function clearLetters(){
-    if(letterIndex==-1){
-        phrasesIndex = (phrasesIndex+1)%phrases.length;
+function clearLetters() {
+    if (letterIndex == -1) {
+        phrasesIndex = (phrasesIndex + 1) % phrases.length;
         letterIndex = 0;
         printLetters(phrases[phrasesIndex]);
     }
-    else if(letterIndex > -1){
+    else if (letterIndex > -1) {
         let updatedPhrase = "";
-        for(let index=0; index<letterIndex; index++){
+        for (let index = 0; index < letterIndex; index++) {
             updatedPhrase += phrases[phrasesIndex].charAt(index);
         }
         dynamicContent.textContent = updatedPhrase;
-        letterIndex -=1;
+        letterIndex -= 1;
         setTimeout(clearLetters, erasingSpeed);
     }
 }
--- theme.js
+++ theme.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     var theme;
     var dispButton;
-    const dark = `<span class="material-symbols-outlined"> toggle_on </span>`;
-    const light = `<span class="material-symbols-outlined"> toggle_off </span>`;
+    const dark = `<span class="material-symbols-outlined" style="font-size: 36px"> toggle_on </span>`;
+    const light = `<span class="material-symbols-outlined" style="font-size: 36px"> toggle_off </span>`;
 
     const themeSwitcher1 = document.getElementById("theme-switcher1");
     const themeSwitcher2 = document.getElementById("theme-switcher2");
@@ -21,32 +21,44 @@ document.addEventListener("DOMContentLoaded", () => {
             if (themePreferenceDark().matches) {
                 if (!(document.body.classList.contains("dark-theme"))) {
                     document.body.classList.add("dark-theme");
-                    document.querySelector(".intro").classList.add("dark-theme");
-                    document.querySelector("#nav-header").classList.add("dark-theme");
-                    document.querySelector(".default-nav").classList.add("dark-theme");
-                    document.querySelector("#ingrid").classList.add("dark-theme");
-                    document.querySelector("#div-sections").classList.add("dark-theme");
-                    document.querySelector(".h2-divs").classList.add("dark-theme");
-                    document.querySelector(".cardsection").classList.add("dark-theme");
-                    document.querySelector("#contact-me form input").classList.add("dark-theme");
-                    document.querySelector("#contact-me form textarea").classList.add("dark-theme");
-                    document.querySelector("#contact-me form #submit").classList.add("dark-theme");
+                    document.querySelector(".intro")?.classList.add("dark-theme");
+                    document.querySelector("#nav-header")?.classList.add("dark-theme");
+                    document.querySelector(".default-nav")?.classList.add("dark-theme");
+                    document.querySelector("#ingrid")?.classList.add("dark-theme");
+                    document.querySelector("#div-sections")?.classList.add("dark-theme");
+                    document.querySelector(".h2-divs")?.classList.add("dark-theme");
+                    document.querySelector(".cardsection")?.classList.add("dark-theme");
+                    document.querySelector("#contact-me form input")?.classList.add("dark-theme");
+                    document.querySelector("#contact-me form textarea")?.classList.add("dark-theme");
+                    document.querySelector("#contact-me form #submit")?.classList.add("dark-theme");
+                    document.querySelector("footer p")?.classList.add("dark-theme");
+                    const contactLabels = document.querySelectorAll("#contact label");
+                    contactLabels?.forEach(element => {
+                        element.classList.add("dark-theme");
+                    });
                 }
                 return "dark";
             }
             else {
                 if (document.body.classList.contains("dark-theme")) {
                     document.body.classList.remove("dark-theme");
-                    document.querySelector(".intro").classList.remove("dark-theme");
-                    document.querySelector("#nav-header").classList.remove("dark-theme");
-                    document.querySelector(".default-nav").classList.remove("dark-theme");
-                    document.querySelector("#ingrid").classList.remove("dark-theme");
-                    document.querySelector("#div-sections").classList.remove("dark-theme");
-                    document.querySelector(".h2-divs").classList.remove("dark-theme");
-                    document.querySelector(".cardsection").classList.remove("dark-theme");
-                    document.querySelector("#contact-me form input").classList.remove("dark-theme");
-                    document.querySelector("#contact-me form textarea").classList.remove("dark-theme");
-                    document.querySelector("#contact-me form #submit").classList.remove("dark-theme");
+                    document.querySelector(".intro")?.classList.remove("dark-theme");
+                    document.querySelector("#nav-header")?.classList.remove("dark-theme");
+                    document.querySelector(".default-nav")?.classList.remove("dark-theme");
+                    document.querySelector("#ingrid")?.classList.remove("dark-theme");
+                    document.querySelector("#div-sections")?.classList.remove("dark-theme");
+                    document.querySelector(".h2-divs")?.classList.remove("dark-theme");
+                    document.querySelector(".cardsection")?.classList.remove("dark-theme");
+                    document.querySelector("#contact-me form input")?.classList.remove("dark-theme");
+                    document.querySelector("#contact-me form textarea")?.classList.remove("dark-theme");
+                    document.querySelector("#contact-me form #submit")?.classList.remove("dark-theme");
+                    document.querySelector("footer p")?.classList.remove("dark-theme");
+                    document.querySelectorAll("#contact label")?.classList.remove("dark-theme");
+                    // const contactLabels = document.querySelectorAll("#contact label");
+                    contactLabels?.forEach(element => {
+                        element.classList.remove("dark-theme");
+                    });
+
                     return "light";
                 }
             }
@@ -62,57 +74,87 @@ document.addEventListener("DOMContentLoaded", () => {
         if (currentTheme === "dark") {
             if (!(document.body.classList.contains("dark-theme"))) {
                 document.body.classList.add("dark-theme");
-                document.querySelector(".intro").classList.add("dark-theme");
-                document.querySelector("#nav-header").classList.add("dark-theme");
-                document.querySelector(".default-nav").classList.add("dark-theme");
-                document.querySelector("#ingrid").classList.add("dark-theme");
-                document.querySelector("#div-sections").classList.add("dark-theme");
-                document.querySelector(".h2-divs").classList.add("dark-theme");
-                document.querySelector(".cardsection").classList.add("dark-theme");
-                document.querySelector("#contact-me form input").classList.add("dark-theme");
-                document.querySelector("#contact-me form textarea").classList.add("dark-theme");
-                document.querySelector("#contact-me form #submit").classList.add("dark-theme");
+                document.querySelector(".intro")?.classList.add("dark-theme");
+                document.querySelector("#nav-header")?.classList.add("dark-theme");
+                document.querySelector(".default-nav")?.classList.add("dark-theme");
+                document.querySelector("#ingrid")?.classList.add("dark-theme");
+                document.querySelector("#div-sections")?.classList.add("dark-theme");
+                document.querySelector(".h2-divs")?.classList.add("dark-theme");
+                document.querySelector(".cardsection")?.classList.add("dark-theme");
+                document.querySelector("#contact-me form input")?.classList.add("dark-theme");
+                document.querySelector("#contact-me form textarea")?.classList.add("dark-theme");
+                document.querySelector("#contact-me form #submit")?.classList.add("dark-theme");
+                document.querySelector("footer p")?.classList.add("dark-theme");
+                // document.querySelectorAll("#contact label")?.classList.add("dark-theme");
+                const contactLabels = document.querySelectorAll("#contact label");
+                contactLabels?.forEach(element => {
+                    element.classList.add("dark-theme");
+                });
+
+
             }
             if ((document.body.classList.contains("light-theme"))) {
                 document.body.classList.remove("dark-theme");
-                document.querySelector(".intro").classList.remove("light-theme");
-                document.querySelector("#nav-header").classList.remove("light-theme");
-                document.querySelector(".default-nav").classList.remove("light-theme");
-                document.querySelector("#ingrid").classList.remove("light-theme");
-                document.querySelector("#div-sections").classList.remove("light-theme");
-                document.querySelector(".h2-divs").classList.remove("light-theme");
-                document.querySelector(".cardsection").classList.remove("light-theme");
-                document.querySelector("#contact-me form input").classList.remove("light-theme");
-                document.querySelector("#contact-me form textarea").classList.remove("light-theme");
-                document.querySelector("#contact-me form #submit").classList.remove("light-theme");
+                document.querySelector(".intro")?.classList.remove("light-theme");
+                document.querySelector("#nav-header")?.classList.remove("light-theme");
+                document.querySelector(".default-nav")?.classList.remove("light-theme");
+                document.querySelector("#ingrid")?.classList.remove("light-theme");
+                document.querySelector("#div-sections")?.classList.remove("light-theme");
+                document.querySelector(".h2-divs")?.classList.remove("light-theme");
+                document.querySelector(".cardsection")?.classList.remove("light-theme");
+                document.querySelector("#contact-me form input")?.classList.remove("light-theme");
+                document.querySelector("#contact-me form textarea")?.classList.remove("light-theme");
+                document.querySelector("#contact-me form #submit")?.classList.remove("light-theme");
+                document.querySelector("footer p")?.classList.remove("light-theme");
+                // document.querySelectorAll("#contact label")?.classList.remove("light-theme");
+                const contactLabels = document.querySelectorAll("#contact label");
+                contactLabels?.forEach(element => {
+                    element.classList.remove("light-theme");
+                });
+
             }
         }
         else {  //if (currentTheme==="light")
             if (document.body.classList.contains("dark-theme")) {
                 document.body.classList.remove("dark-theme");
-                document.querySelector(".intro").classList.remove("dark-theme");
-                document.querySelector("#nav-header").classList.remove("dark-theme");
-                document.querySelector(".default-nav").classList.remove("dark-theme");
-                document.querySelector("#ingrid").classList.remove("dark-theme");
-                document.querySelector("#div-sections").classList.remove("dark-theme");
-                document.querySelector(".h2-divs").classList.remove("dark-theme");
-                document.querySelector(".cardsection").classList.remove("dark-theme");
-                document.querySelector("#contact-me form input").classList.remove("dark-theme");
-                document.querySelector("#contact-me form textarea").classList.remove("dark-theme");
-                document.querySelector("#contact-me form #submit").classList.remove("dark-theme");
+                document.querySelector(".intro")?.classList.remove("dark-theme");
+                document.querySelector("#nav-header")?.classList.remove("dark-theme");
+                document.querySelector(".default-nav")?.classList.remove("dark-theme");
+                document.querySelector("#ingrid")?.classList.remove("dark-theme");
+                document.querySelector("#div-sections")?.classList.remove("dark-theme");
+                document.querySelector(".h2-divs")?.classList.remove("dark-theme");
+                document.querySelector(".cardsection")?.classList.remove("dark-theme");
+                document.querySelector("#contact-me form input")?.classList.remove("dark-theme");
+                document.querySelector("#contact-me form textarea")?.classList.remove("dark-theme");
+                document.querySelector("#contact-me form #submit")?.classList.remove("dark-theme");
+                document.querySelector("footer p")?.classList.remove("dark-theme");
+                // document.querySelectorAll("#contact label")?.classList.remove("dark-theme");
+                const contactLabels = document.querySelectorAll("#contact label");
+                contactLabels?.forEach(element => {
+                    element.classList.remove("dark-theme");
+                });
+
+
             }
             if (!document.body.classList.contains("light-theme")) {
                 document.body.classList.add("light-theme");
-                document.querySelector(".intro").classList.add("light-theme");
-                document.querySelector("#nav-header").classList.add("light-theme");
-                document.querySelector(".default-nav").classList.add("light-theme");
-                document.querySelector("#ingrid").classList.add("light-theme");
-                document.querySelector("#div-sections").classList.add("light-theme");
-                document.querySelector(".h2-divs").classList.add("light-theme");
-                document.querySelector(".cardsection").classList.add("light-theme");
-                document.querySelector("#contact-me form input").classList.add("light-theme");
-                document.querySelector("#contact-me form textarea").classList.add("light-theme");
-                document.querySelector("#contact-me form #submit").classList.add("light-theme");
+                document.querySelector(".intro")?.classList.add("light-theme");
+                document.querySelector("#nav-header")?.classList.add("light-theme");
+                document.querySelector(".default-nav")?.classList.add("light-theme");
+                document.querySelector("#ingrid")?.classList.add("light-theme");
+                document.querySelector("#div-sections")?.classList.add("light-theme");
+                document.querySelector(".h2-divs")?.classList.add("light-theme");
+                document.querySelector(".cardsection")?.classList.add("light-theme");
+                document.querySelector("#contact-me form input")?.classList.add("light-theme");
+                document.querySelector("#contact-me form textarea")?.classList.add("light-theme");
+                document.querySelector("#contact-me form #submit")?.classList.add("light-theme");
+                document.querySelector("footer p")?.classList.add("light-theme");
+                // document.querySelectorAll("#contact label")?.classList.add("light-theme");  
+                const contactLabels = document.querySelectorAll("#contact label");
+                contactLabels?.forEach(element => {
+                    element.classList.add("light-theme");
+                });
+
             }
         }
         console.log("currentTheme for button: ", currentTheme);
@@ -134,28 +176,41 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
         document.body.classList.toggle("dark-theme");
-        document.querySelector(".intro").classList.toggle("dark-theme");
-        document.querySelector("#nav-header").classList.toggle("dark-theme");
-        document.querySelector(".default-nav").classList.toggle("dark-theme");
-        document.querySelector("#ingrid").classList.toggle("dark-theme");
-        document.querySelector("#div-sections").classList.toggle("dark-theme");
-        document.querySelector(".h2-divs").classList.toggle("dark-theme");
-        document.querySelector(".cardsection").classList.toggle("dark-theme");
-        document.querySelector("#contact-me form input").classList.toggle("dark-theme");
-        document.querySelector("#contact-me form textarea").classList.toggle("dark-theme");
-        document.querySelector("#contact-me form #submit").classList.toggle("dark-theme");
+        document.querySelector(".intro")?.classList.toggle("dark-theme");
+        document.querySelector("#nav-header")?.classList.toggle("dark-theme");
+        document.querySelector(".default-nav")?.classList.toggle("dark-theme");
+        document.querySelector("#ingrid")?.classList.toggle("dark-theme");
+        document.querySelector("#div-sections")?.classList.toggle("dark-theme");
+        document.querySelector(".h2-divs")?.classList.toggle("dark-theme");
+        document.querySelector(".cardsection")?.classList.toggle("dark-theme");
+        document.querySelector("#contact-me form input")?.classList.toggle("dark-theme");
+        document.querySelector("#contact-me form textarea")?.classList.toggle("dark-theme");
+        document.querySelector("#contact-me form #submit")?.classList.toggle("dark-theme");
+        document.querySelector("footer p")?.classList.toggle("dark-theme");
+        // document.querySelectorAll("#contact label")?.classList.toggle("dark-theme");
+        const contactLabels = document.querySelectorAll("#contact label");
+        contactLabels?.forEach(element => {
+            element.classList.toggle("dark-theme");
+        });
+
+
 
         document.body.classList.toggle("light-theme");
-        document.querySelector(".intro").classList.toggle("light-theme");
-        document.querySelector("#nav-header").classList.toggle("light-theme");
-        document.querySelector(".default-nav").classList.toggle("light-theme");
-        document.querySelector("#ingrid").classList.toggle("light-theme");
-        document.querySelector("#div-sections").classList.toggle("light-theme");
-        document.querySelector(".h2-divs").classList.toggle("light-theme");
-        document.querySelector(".cardsection").classList.toggle("light-theme");
-        document.querySelector("#contact-me form input").classList.toggle("light-theme");
-        document.querySelector("#contact-me form textarea").classList.toggle("light-theme");
-        document.querySelector("#contact-me form #submit").classList.toggle("light-theme");
+        document.querySelector(".intro")?.classList.toggle("light-theme");
+        document.querySelector("#nav-header")?.classList.toggle("light-theme");
+        document.querySelector(".default-nav")?.classList.toggle("light-theme");
+        document.querySelector("#ingrid")?.classList.toggle("light-theme");
+        document.querySelector("#div-sections")?.classList.toggle("light-theme");
+        document.querySelector(".h2-divs")?.classList.toggle("light-theme");
+        document.querySelector(".cardsection")?.classList.toggle("light-theme");
+        document.querySelector("#contact-me form input")?.classList.toggle("light-theme");
+        document.querySelector("#contact-me form textarea")?.classList.toggle("light-theme");
+        document.querySelector("#contact-me form #submit")?.classList.toggle("light-theme");
+        document.querySelector("footer p")?.classList.toggle("light-theme");
+        // document.querySelectorAll("#contact label")?.classList.toggle("light-theme");
+        contactLabels?.forEach(element => {
+            element.classList.toggle("light-theme");
+        });
 
         localStorage.setItem("theme", theme);
         setButtonText(theme);
